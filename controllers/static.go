aa0f1781878ecc3d29b2836c0354951e2343d39c
gapichardo <gapichardo@gmail.com>
2022-01-26 17:19:04 -0600

    Se agregaron componentes

--- controllers/static.go
+++ controllers/static.go
@@ -1,1 +1,19 @@
 package controllers
+
+import "lenslocked.com/views"
+
+func NewStatic() *Static {
+	return &Static{
+		Home:     views.NewView("bootstrap", "static/home"),
+		Contact:  views.NewView("bootstrap", "static/contact"),
+		Faq:      views.NewView("bootstrapFAQ", "static/faq"),
+		NotFound: views.NewView("bootstrap", "static/notFound"),
+	}
+}
+
+type Static struct {
+	Home     *views.View
+	Contact  *views.View
+	Faq      *views.View
+	NotFound *views.View
+}
--- main.go
+++ main.go
@@ -5,55 +5,21 @@ import (
 
 	"github.com/gorilla/mux"
 	"lenslocked.com/controllers"
-	"lenslocked.com/views"
-)
-
-var (
-	homeView     *views.View
-	contactView  *views.View
-	faqView      *views.View
-	notFounfView *views.View
 )
 
 func main() {
-	var h http.Handler = http.HandlerFunc(notfoundFunc)
-	homeView = views.NewView("bootstrap", "views/home.gohtml")
-	contactView = views.NewView("bootstrap", "views/contact.gohtml")
-	faqView = views.NewView("bootstrapFAQ", "views/faq.gohtml")
-	notFounfView = views.NewView("bootstrap", "views/notFound.gohtml")
 	usersC := controllers.NewUsers()
+	staticC := controllers.NewStatic()
+	var h http.Handler = http.Handler(staticC.NotFound)
+
 	r := mux.NewRouter()
-	r.HandleFunc("/", home).Methods("GET")
-	r.HandleFunc("/contact", contact).Methods("GET")
+	r.Handle("/", staticC.Home).Methods("GET")
+	r.Handle("/contact", staticC.Contact).Methods("GET")
+	r.Handle("/faq", staticC.Faq).Methods("GET")
+
 	r.HandleFunc("/signup", usersC.New).Methods("GET")
 	r.HandleFunc("/signup", usersC.Create).Methods("POST")
-	r.HandleFunc("/faq", faq).Methods("GET")
+
 	r.NotFoundHandler = h
 	http.ListenAndServe(":3000", r)
-}
-
-func home(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	must(homeView.Render(w, nil))
-}
-
-func contact(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	must(contactView.Render(w, nil))
-}
-func faq(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	must(faqView.Render(w, nil))
-}
-
-func notfoundFunc(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	must(notFounfView.Render(w, nil))
-}
-
-// A helper function that panics on any error
-func must(err error) {
-	if err != nil {
-		panic(err)
-	}
 }
